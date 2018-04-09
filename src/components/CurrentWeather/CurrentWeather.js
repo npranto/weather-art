32774612596e97a30cc514455822ce98a89a7650
Weatherion <Weatherion@Nazmuzs-MacBook-Air.local>
2018-04-09 11:25:54 -0400

    centers app content for large screens

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -6,7 +6,7 @@ class CurrentWeather extends Component {
     render() {
         return (
             <div className="CurrentWeather">
-                <p className="city"> San Francisco </p>
+                <p className="city"> San Francisco Bay Area </p>
                 <p className="condition"> Partly Cloudy </p>
                 <p className="temperature"> 54&deg; </p>
             </div>
