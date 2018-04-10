8446e32552f65e9006a0622b862e9847a7c9f2da
Weatherion <Weatherion@nat-wireless-guest-reg-153-50.bu.edu>
2018-04-10 13:13:48 -0400

    w-WoVHxsci => Layout for TodayHighAndLow component

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import CurrentWeather from '../CurrentWeather/CurrentWeather';
+import TodayHighAndLow from '../TodayHighAndLow/TodayHighAndLow';
 import './Weather.css';
 
 class Weather extends Component {
@@ -8,6 +9,7 @@ class Weather extends Component {
         return (
             <div className="Weather">
                 <CurrentWeather />
+                <TodayHighAndLow />
             </div>
         )
     }
