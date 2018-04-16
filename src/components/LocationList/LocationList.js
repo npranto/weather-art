f3ca8b1ceb926bf98b1ad727d32f7cc85dbac747
Weatherion <Weatherion@nat-wireless-guest-reg-153-50.bu.edu>
2018-04-16 10:21:42 -0400

    w-DTGAkZio => Layout for LocationList component

---
+++ src/components/LocationList/LocationList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+import './LocationList.css';
+
+class LocationList extends Component {
+    render() {
+        return (
+            <div className="LocationList">
+                Location List 
+            </div>
+        )
+    }
+}
+
+export default LocationList;
