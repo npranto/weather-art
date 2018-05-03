f67307b7a7ea5f459dd126279e3a02e02354910c
Weatherion <Weatherion@Nazmuzs-MacBook-Air.local>
2018-05-03 13:22:24 -0400

    w-40645f1jgminup3 => Fetch and display data for TodayHighAndLow component

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -18,24 +18,28 @@ class Weather extends Component {
         this.props.getTodaysHighAndLowTemperatures();
     }
     render() {
-        const { condition } = this.props.weather;
-        const { forecastToday } = this.props.weather;
-        console.log(forecastToday);
+        const { condition, forecastToday } = this.props.weather;
         return (
             <div className="Weather">
-                <CurrentWeather 
-                    city={condition.city} 
-                    temperature={condition.temp_f} 
-                    condition={condition.weather} />
-                {
-                    forecastToday 
-                        ?   <TodayHighAndLow 
-                                day={forecastToday.todaysHighAndLow.day}
-                                month={forecastToday.todaysHighAndLow.month}
-                                weekday={forecastToday.todaysHighAndLow.weekday}
-                                highTemperature={forecastToday.todaysHighAndLow.highF}
-                                lowTemperature={forecastToday.todaysHighAndLow.lowF} />
-                        : ''
+                { condition 
+                    ? (
+                        <CurrentWeather 
+                            city={condition.city} 
+                            temperature={condition.temp_f} 
+                            condition={condition.weather} />
+                    )
+                    : (<div> Loading... </div>)
+                }
+                { forecastToday && forecastToday.todaysHighAndLow 
+                    ? (
+                        <TodayHighAndLow 
+                            day={forecastToday.todaysHighAndLow.day}
+                            month={forecastToday.todaysHighAndLow.month}
+                            weekday={forecastToday.todaysHighAndLow.weekday}
+                            highTemperature={forecastToday.todaysHighAndLow.highF}
+                            lowTemperature={forecastToday.todaysHighAndLow.lowF} />
+                    )
+                    : (<div> Loading... </div>)
                 }
                 <HourlyForecast />
                 <WeeklyForecast />
