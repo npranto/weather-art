52b821651c4f7f164d9246041aad59eb1d90b9c7
Weatherion <Weatherion@Nazmuzs-MacBook-Air.local>
2018-05-07 18:42:46 -0400

    w-ef0ew1jgwt1rg1 => Create currentActiveLocationReducer with state: {coordinates: {}}

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import {combineReducers} from 'redux';
 import findLocationReducer from './findLocationReducer';
 import weatherReducer from './weatherReducer';
 import weatherGalleryReducer from './weatherGalleryReducer';
+import currentActiveLocationReducer from './currentActiveLocationReducer';
 
 const reducers = combineReducers({
     findLocation: findLocationReducer,
     weather: weatherReducer,
-    weatherGallery: weatherGalleryReducer
+    weatherGallery: weatherGalleryReducer,
+    currentActiveLocation: currentActiveLocationReducer
 })
 
 export default reducers;
